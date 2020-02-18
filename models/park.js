1b64405e15582889043499e8a3a9cceca67af71b
mholwill <m_holwill@hotmail.co.uk>
2020-02-18 16:04:07 +0000

    Initial Commit

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, ticketPrice, collectionOfDinosaurs){
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+  this.collectionOfDinosaurs = collectionOfDinosaurs;
+}
+
+module.export = Park;
